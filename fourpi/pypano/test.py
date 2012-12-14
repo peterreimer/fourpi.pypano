f78cd7a40aaff619ef29b66b232f58b77d86d8c6
Peter Reimer <peter@4pi.org>
2012-12-14 20:18:23 +0100

    extract six cubic faces

--- fourpi/pypano/test.py
+++ fourpi/pypano/test.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env  python
-import PIL.Image
 import extract
 
-image = PIL.Image.open("pano.jpg")
-pano = extract.Extractor(image,views=[])
-print pano.get_extracted_views()
-print pano.save()
+faces = (
+    (   0,  0,0,90, "f"), #front
+    (  90,  0,0,90, "l"), #left
+    ( -90,  0,0,90, "r"), #right
+    (-180,  0,0,90, "b"), #back
+    (   0,-90,0,90, "u"), #up
+    (   0, 90,0,90, "d")  #down
+)
+views = []
+for yaw, pitch, roll, fov, pos in faces:
+    views.append(extract.View(yaw, pitch, roll, fov))
+
+pano_file = open("pano.jpg","rb")
+pano = extract.Extractor(pano_file,views=views)
+pano.extract()
+
+for view in pano.views:
+    print view.image.size
