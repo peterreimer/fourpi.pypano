611a43199bbc346991c6f4907cccdb6a2e2e5235
Peter Reimer <peter@4pi.org>
2014-09-23 21:11:00 +0200

    panosalado configuration

--- fourpi/pypano/config.py
+++ fourpi/pypano/config.py
@@ -27,34 +27,57 @@ class Panorama:
 
 class OpenPanoConfigurationMaker:
 
-    def __init__(self, debug=False):
+    def __init__(self, config_file, debug=False):
         """bla"""
+        tree = ET.ElementTree(file=config_file)
+        salado = tree.getroot()
+        
+        glob = salado.find('global')
+        glob.set('debug', str(debug)) 
+        if not debug:
+            print "delete ViewFinder"
+            modules = salado.find('modules')
+            viewfinder = modules.find('ViewFinder')
+            if viewfinder:
+                modules.remove(viewfinder)
+        self.salado = salado
+
+    def _make_view(self, pan, tilt, fov):
+        
+        view = []
+        if pan:
+            view.append('pan:%.1f' % pan)
+        if tilt:
+            view.append('tilt:%.1f' % tilt)
+        if fov:
+            view.append('fov:%.1f' % fov)
+        return ','.join(view)
     
-        sp =  ET.Element("SaladoPlayer")
-
-        glob = ET.SubElement(sp, "global", debug=str(debug))
-        panoramas = ET.SubElement(sp, "panoramas")
-        modules = ET.SubElement(sp, "modules")
-        actions= ET.SubElement(sp, "actions")
-
-        self.xml = sp
-
-    def add_panorama(self, id, path, direction=0, **kwargs):
+    def add_panorama(self, pano_id, path, direction="0", **kwargs):
         """sfd"""
+        salado = self.salado
         pan = kwargs.get('pan', 0)
         tilt = kwargs.get('tilt', 0)
         fov = kwargs.get('fov', 100)
+        pano_attributes = {}
+        pano_attributes['id'] = pano_id  
+        pano_attributes['path'] = path 
+        pano_attributes['view'] = self._make_view(pan, tilt, fov)
+        panoramas = salado.find('panoramas')
+        panorama =  ET.SubElement(panoramas, "panorama", attrib=pano_attributes)
         
-        #pano =  ET.Element("panorama", id=id, path=path, direction=direction)
-        print self.xml.get('panoramas')
+        self.salado = salado
+
 
     def get_conf(self):
-        xml = self.xml
-        indent(xml)
-        return ET.tostring(xml, encoding='utf-8')
+        salado = self.salado
+        indent(salado)
+        return ET.tostring(salado, encoding='utf-8')
 
 if __name__ == '__main__':
 
-    conf = OpenPanoConfigurationMaker(debug=True)
+
+    conf = OpenPanoConfigurationMaker('salado.xml', debug=True)
     conf.add_panorama('zeughaus', 'pano/zeughaus_f.xml')
+    conf.add_panorama('laschozas', 'pano/laschozas_f.xml', pan=123.4, tilt=15)
     print conf.get_conf()
