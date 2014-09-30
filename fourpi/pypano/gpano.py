0496a6e0e40d14b686f404262f6ba4c901167016
Peter Reimer <peter@4pi.org>
2014-09-30 21:42:50 +0200

    some renaming and checking

--- fourpi/pypano/gpano.py
+++ fourpi/pypano/gpano.py
@@ -1,30 +1,39 @@
 import json
+import os
 import subprocess
 from config import OpenPanoConfigurationMaker
+#from fourpi.pypano.size import pano
 
 EXIFTOOL = "exiftool"
 
-def dummy(image):
+def get_exif(image):
     #pipe = subprocess.Popen(EXIFTOOL + " " + image, shell=True, stderr=subprocess.PIPE)
     #output = pipe.stderr.read()
-    exif = subprocess.check_output([EXIFTOOL, '-j', image])
-    return json.loads(exif)[0]
-
+    if os.path.isfile(image):
+        exif = subprocess.check_output([EXIFTOOL, '-j', image])
+        return json.loads(exif)[0]
+    else:
+        return None
 
 
 if __name__ == '__main__':
 
-    #exif = dummy('/home/reimer/Bilder/Panoramen/schaukel.jpg')
-    exif = dummy('/home/reimer/Bilder/Panoramen/anonymous.jpg')
-    for k in exif.keys():
-        print k
-    direction = exif.get('PoseHeadingDegrees', None)
-    pan = exif.get('InitialViewHeadingDegrees', None)
-    tilt = exif.get('InitialViewPitchDegrees',None)
-    fov = exif.get('InitialHorizontalFOVDegrees', None)
-    
     conf = OpenPanoConfigurationMaker(debug=True)
-    conf.add_panorama('laschozas', 'pano/laschozas_f.xml', pan=pan, tilt=tilt, fov=fov, direction=direction)
+
+    #exif = dummy('/home/reimer/Bilder/Panoramen/schaukel.jpg')
+    panos = ('/home/reimer/Bilder/Panoramen/anonymous.jpg',
+             '/home/reimer/Bilder/Panoramen/markt-eq.jpg'  )
+    for pano in panos:
+        if os.path.isfile(pano):
+            filename = os.path.basename(pano)
+            shortname, extension = os.path.splitext(filename)
+            exif = get_exif(pano)
+            direction = exif.get('PoseHeadingDegrees', None)
+            pan = exif.get('InitialViewHeadingDegrees', None)
+            tilt = exif.get('InitialViewPitchDegrees',None)
+            fov = exif.get('InitialHorizontalFOVDegrees', None)
+        
+            conf.add_panorama(shortname, '%s/%s_f.xml' % (shortname, shortname), pan=pan, tilt=tilt, fov=fov, direction=direction)
     print conf.get_conf()
 
     
